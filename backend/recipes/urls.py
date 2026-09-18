51301997a6c04921dbb2bc97de06739b99cc330f
dimtry <bestpythondev@gmail.com>
2026-09-18 18:56:28 +0300

    Add RecipeViewSet with filters

--- backend/recipes/urls.py
+++ backend/recipes/urls.py
@@ -1,9 +1,10 @@
 from rest_framework.routers import DefaultRouter
 
-from .views import IngredientViewSet, TagViewSet
+from .views import IngredientViewSet, TagViewSet, RecipeViewSet
 
 router = DefaultRouter()
 router.register('tags', TagViewSet, basename='tag')
 router.register('ingredients', IngredientViewSet, basename='ingredient')
+router.register('recipes', RecipeViewSet, basename='recipe')
 
 urlpatterns = router.urls
--- backend/recipes/views.py
+++ backend/recipes/views.py
@@ -1,7 +1,17 @@
 from rest_framework.viewsets import ReadOnlyModelViewSet
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.viewsets import ModelViewSet
 
-from .models import Ingredient, Tag
-from .serializers import IngredientSerializer, TagSerializer
+from users.permissions import IsAuthorOrReadOnly
+
+from .models import Ingredient, Tag, Recipe
+from .serializers import (
+    IngredientSerializer,
+    RecipeCreateSerializer,
+    RecipeListSerializer,
+    RecipeUpdateSerializer,
+    TagSerializer,
+)
 
 
 class TagViewSet(ReadOnlyModelViewSet):
@@ -24,4 +34,44 @@ class IngredientViewSet(ReadOnlyModelViewSet):
         name = self.request.query_params.get('name')
         if name:
             qs = qs.filter(name__startswith=name)
+        return qs
+
+
+class RecipeViewSet(ModelViewSet):
+    permission_classes = (IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly)
+
+    def get_serializer_class(self):
+        if self.action == 'create':
+            return RecipeCreateSerializer
+        if self.action in ('update', 'partial_update'):
+            return RecipeUpdateSerializer
+        return RecipeListSerializer
+
+    def get_queryset(self):
+        qs = Recipe.objects.select_related('author').prefetch_related(
+            'recipe_ingredients__ingredient',
+            'tags',
+        )
+        params = self.request.query_params
+
+        if params.get('is_favorited') == '1':
+            if self.request.user.is_authenticated:
+                qs = qs.filter(favorited_by__user=self.request.user)
+            else:
+                qs = qs.none()
+
+        if params.get('is_in_shopping_cart') == '1':
+            if self.request.user.is_authenticated:
+                qs = qs.filter(in_shopping_carts__user=self.request.user)
+            else:
+                qs = qs.none()
+
+        author_id = params.get('author')
+        if author_id:
+            qs = qs.filter(author_id=author_id)
+
+        tags = params.getlist('tags')
+        if tags:
+            qs = qs.filter(tags__slug__in=tags).distinct()
+
         return qs
